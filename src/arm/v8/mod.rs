4920397ac26ef08feafe475ee9de1a7a3db25d7d
gnzlbg <gonzalobg88@gmail.com>
2017-01-31 15:05:33 +0100

    re-enable clippy doc_markdown warning

--- src/arm/v8/mod.rs
+++ src/arm/v8/mod.rs
@@ -1,6 +1,5 @@
 //! ARMv8 assembly instructions.
 //!
-//! The reference is [ARMv8-A Reference Manual (issue
-//! A.k_iss10775)](http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0487a.k_10775/index.html).
+//! The reference is [ARMv8-A Reference Manual](http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0487a.k_10775/index.html).
 
 pub use alg::arm::v8::*;
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 #![cfg_attr(RUSTC_IS_NIGHTLY, feature(i128_type))]
 #![cfg_attr(RUSTC_IS_NIGHTLY, feature(link_llvm_intrinsics))]
 
-#![cfg_attr(feature = "cargo-clippy", allow(doc_markdown))]
 #![no_std]
 
 //! `bitintr` offers portable bit manipulation intrinsics
